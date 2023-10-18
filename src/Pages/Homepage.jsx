218316918229ca586361fe153c7ec6ae50f7f9c8
frs11~ <fayezurrahman017@gmail.com>
2023-10-18 16:50:53 +0600

    added footer

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import Banner from "../Components/Homepage/Banner";
+import Footer from "../Components/Homepage/Footer";
 
 const Homepage = () => {
   return (
     <div className="">
       <Banner></Banner>
+      <Footer></Footer>
     </div>
   );
 };
